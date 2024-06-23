b46cc6ea6d30a980d569ca8440b44fc44aca3a1c
Ubuntu <ubuntu@ip-172-31-12-179.ap-south-1.compute.internal>
2024-06-23 09:27:42 +0000

    code change

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,13 @@ if [ "$BRANCH_NAME" = "dev_branch" ]; then
    echo "Build and deploying code in Development "
    ./build.sh
    docker tag nginximg susidockerrepo/dev:latest
-   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PAT"
+   #docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PAT"
    docker push susidockerrepo/dev:latest
 elif [ "$BRANCH_NAME" = "main" ]; then
    echo "Build and deploying code in production "
    ./build.sh
    docker tag nginximg susidockerrepo/prod:latest
-   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PAT"
+   #docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PAT"
    docker push susidockerrepo/prod:latest
 else
    echo "Deployment failed..."
