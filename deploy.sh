97e473202a17fce3d7743e22a8c7d54aa04010c6
Ubuntu <ubuntu@ip-172-31-12-179.ap-south-1.compute.internal>
2024-06-23 05:46:40 +0000

    Adding scripts

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,15 @@ if [[ "$GIT_BRANCH" == 'dev_branch' ]]; then
    echo "Build and deploying code in Development "
    ./build.sh
    docker tag nginximg susidockerrepo/dev:latest
-   #docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PAT"
-   docker.withRegistry('https://index.docker.org', 'Capstone_project_pipeline') {
+   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PAT"
    docker push susidockerrepo/dev:latest
-}
 elif [[ "$GIT_BRANCH" == 'main' ]]; then
    echo "Build and deploying code in production "
    ./build.sh
    docker tag nginximg susidockerrepo/prod:latest
-   #docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PAT"
-   docker.withRegistry('https://index.docker.org', 'Capstone_project_pipeline') {
+   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PAT"
    docker push susidockerrepo/prod:latest
-}
 else
-   echo "Deployment failed"
+   echo "Deployment failed..."
 fi
 
