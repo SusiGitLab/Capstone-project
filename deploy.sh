d21d3f62084ffd45be13d1581a01c1393d46f73f
Ubuntu <ubuntu@ip-172-31-12-179.ap-south-1.compute.internal>
2024-06-23 07:16:19 +0000

    code change

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,13 @@
 set -x #Enable debugging output
 
 
-if [[ "$BRANCH_NAME" == 'dev_branch' ]]; then
+if [[ "$BRANCH_NAME" == "dev_branch" ]]; then
    echo "Build and deploying code in Development "
    ./build.sh
    docker tag nginximg susidockerrepo/dev:latest
    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PAT"
    docker push susidockerrepo/dev:latest
-elif [[ "$BRANCH_NAME" == 'main' ]]; then
+elif [[ "$BRANCH_NAME" == "main" ]]; then
    echo "Build and deploying code in production "
    ./build.sh
    docker tag nginximg susidockerrepo/prod:latest
