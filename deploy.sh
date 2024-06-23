cac9e8b3bfa921944b9ada83cf2cbdf86afcb734
Ubuntu <ubuntu@ip-172-31-12-179.ap-south-1.compute.internal>
2024-06-23 06:47:53 +0000

    updating scripts

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,14 @@
 
 set -x #Enable debugging output
 
-GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo $GIT_BRANCH
 
-if [[ "$GIT_BRANCH" == 'dev_branch' ]]; then
+if [[ "$BRANCH_NAME" == 'dev_branch' ]]; then
    echo "Build and deploying code in Development "
    ./build.sh
    docker tag nginximg susidockerrepo/dev:latest
    docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PAT"
    docker push susidockerrepo/dev:latest
-elif [[ "$GIT_BRANCH" == 'main' ]]; then
+elif [[ "$BRANCH_NAME" == 'main' ]]; then
    echo "Build and deploying code in production "
    ./build.sh
    docker tag nginximg susidockerrepo/prod:latest
